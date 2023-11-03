871a40a9276f58fd0061f1f5194ad71024332ee5
Fausto-Briones <88986214+Fausto-Briones@users.noreply.github.com>
2023-11-03 10:56:42 -0500

    Codigo que valida input de datos

--- Componentes/AnalizadorLexico/main.py
+++ Componentes/AnalizadorLexico/main.py
@@ -1,3 +1,16 @@
-//Programa que recibe codigo en Rust
+#Programa que recibe codigo en Rust
+import ply.lex as lex
+import functions,tokens, reserved_words,symbols_regex
+
 if __name__ == "__main__":
-  //Codigo
+  lexer = lex.lex()
+  entrada = input(code)
+  while(r"[^END]"):
+    lexer.input(entrada)
+  # Enviando el código
+  lexer.input(code)
+
+  # Tokenizar
+  for token in lexer:
+    print(token)
+  
