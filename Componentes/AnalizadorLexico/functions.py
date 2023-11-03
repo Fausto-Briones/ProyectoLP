15550f8b148c7011176161a7eab0e7cf2dcf212c
Fausto-Briones <88986214+Fausto-Briones@users.noreply.github.com>
2023-11-03 10:44:00 -0500

    Funciones de tipos de datos
    Actualizarlas con los tipos de datos de rust

--- Componentes/AnalizadorLexico/functions.py
+++ Componentes/AnalizadorLexico/functions.py
@@ -1,1 +1,37 @@
 //LLENAR CON FUNCIONES POR TOKEN
+#@author Fausto-Briones
+import ply.lex as lex
+
+def t_IDENTIFIER(t):
+  r'[a-zA-Z_][a-zA-Z_0-9]*'
+  t.type = reserved.get(t.value,'IDENTIFIER')
+  return t
+  
+# Expresión regular para números flotantes, incluye cast
+def t_FLOAT(t):
+  r'(\d+\.\d+)|(\d+\.)'
+  t.value = float(t.value)
+  return t
+
+# Expresion regular para enteros
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+# Expresión regular para reconocer strings
+
+def t_STRING(t):
+  r'\".*?\"'
+  t.value = t.value[1:-1]
+  return t
+
+#Salto de linea
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# Token de error
+def t_error(t):
+  print(f"{t.type.upper()}: No se reconoce el caracter {t.value[0]} en la línea {t.lineno}")
+  t.lexer.skip(1)
