e580366dc660a4bba1c2995043e5967f006150bc
Fausto-Briones <88986214+Fausto-Briones@users.noreply.github.com>
2023-11-03 10:38:25 -0500

    Update symbols_regex.py

--- Componentes/AnalizadorLexico/symbols_regex.py
+++ Componentes/AnalizadorLexico/symbols_regex.py
@@ -1,1 +1,28 @@
+//@author Fausto-Briones
+import ply.lex as lex
+
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_MOD = r"%"
+t_EQ = r"=="
+t_LEQ = r"<="
+t_GEQ = r">="
+t_NEQ = r"!="
+t_INTD = r"//"
+t_ASIG = r"="
+t_LCORCH = r"["
+t_RCORCH = r"]"
+t_LLLAVE = r"{"
+t_RLLAVE = r"}"
+t_DOT = r"."
+t_COLON = r":"
+t_DOUBLE_COLON = r"::"
+t_COMMA = r","
+t_QUOTATION_MARK = r"'"
+t_DOUBLE_QUOTATION_MARK = r'"'
+
 //LLENAR
