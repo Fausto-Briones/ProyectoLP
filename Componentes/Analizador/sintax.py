8cde86a73a9b19cf9a016b87f379d9a619d549be
Axcel Villagran <avglatina1@outlook.com>
2023-11-13 22:05:13 -0500

    Regla para arreglos agregada

--- Componentes/Analizador/sintax.py
+++ Componentes/Analizador/sintax.py
@@ -1,39 +1,65 @@
 import ply.yacc as sint
-from AnalizadorLexico.tokens import tokens
+from tokens import tokens
+from main import *
+
 
 def p_programa(p):
-  'programa : sentencias | sentencias programa'
+  '''programa : sentencias
+              | sentencias programa
+  '''
 
 def p_asignacion(p):
   'asignacion : LET IDENTIFIER EQ valor'
 
 def p_valor(p):
   '''
-  valor : INTEGER | FLOAT | IDENTIFIER
+  valor : INTEGER 
+          | FLOAT 
+          | IDENTIFIER
   
   '''
 def p_sentencias(p):
   '''
-  sentencias : asignacion | impresion
+  sentencias : asignacion SEMICOLON
+              | impresion SEMICOLON
+              | funcion SEMICOLON
+              | ingreso_datos SEMICOLON
+              | arreglos SEMICOLON
+
   '''
 def p_impresion(p):
   '''
-  impresion : PRINTLN LPAREN ( " IDENTIFIER | impresion_valor " ) RPAREN
+  impresion : PRINTLN LPAREN valor RPAREN
   
   '''
 
 def p_funcion(p):
   '''
-  funcion: FN IDENTIFIER LPAR RPAR LLLAVE programa RLLAVE
+  funcion : FN IDENTIFIER LPAREN RPAREN LLLAVE programa RLLAVE
   
   '''
 
 def p_ingreso(p):
   '''
-  ingreso_datos: STD DOUBLE_COLON IO DOUBLE_COLON DOT STDIN LPAREN RPAREN DOT READLINE LPAREN REFERENCE MUT IDENTIFIER RPAREN"
+  ingreso_datos : STD DOUBLE_COLON IO DOUBLE_COLON DOT STDIN LPAREN RPAREN DOT READLINE LPAREN REFERENCE MUT IDENTIFIER RPAREN
+
+  '''
+
+def p_arreglos(p):
+  '''
+  arreglos : LCORCH valor RCORCH
+
   '''
 
 def p_error(p):
   print(f"Error sintáctico en la entrada {p}")
 
 parser = sint.yacc()
+while True:
+   try:
+       s = input('esp > ')
+   except EOFError:
+       break
+   if not s: continue
+   result = parser.parse(s)
+   print(result)
