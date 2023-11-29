6fd19f889f87d464644ca84012edcc4fee96496a
Fausto-Briones <88986214+Fausto-Briones@users.noreply.github.com>
2023-11-29 10:43:31 -0500

    Update interface.py

--- interfaz/interface.py
+++ interfaz/interface.py
@@ -2,4 +2,8 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Analizador Rust")
 window.geometry("300x200")
+text = tk.Label(window, text="Ingresa el codigo de Rust: ")
+text.pack()
+entry = tk.Text(window)
+entry.pack()
 window.mainloop()
