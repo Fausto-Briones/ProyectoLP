ac6d814dafa6cee68d013a521c3f8f75f5800932
Fausto-Briones <88986214+Fausto-Briones@users.noreply.github.com>
2023-11-29 10:46:30 -0500

    Update interface.py

--- interfaz/interface.py
+++ interfaz/interface.py
@@ -6,4 +6,12 @@ text = tk.Label(window, text="Ingresa el codigo de Rust: ")
 text.pack()
 entry = tk.Text(window)
 entry.pack()
+button = tk.Button(window,
+    text="Validar",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+)
+button.pack()
 window.mainloop()
